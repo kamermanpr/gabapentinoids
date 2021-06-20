0980e1c1ee78cfd3b03455fdc688c66de40ba4d7
Peter Kamerman <peter.kamerman@gmail.com>
2021-06-20 12:30:43 +0200

    Updated to include "quantity", 2021-06-20

--- 01_clean-data.R
+++ 01_clean-data.R
@@ -9,7 +9,7 @@ library(readr)
 ############################################################
 
 # Fetching and cleaning the data only needs to be done once.
-# Date completed: 2021-06-17
+# Date completed: 2021-06-20
 
 ###############################################
 #   Fetch the data from openprescribing.net   #
@@ -19,16 +19,18 @@ library(readr)
 # Drug codes can be obtained here: https://openprescribing.net/analyse/
 
 #-- Download pregabalin data (chemical designation only) --#
-# Total items prescribed
+# Total items prescribed and quantity of tablets/capsules
 # Code: 0408010AE
 pregabalin <- read_csv('https://openprescribing.net/api/1.0/spending/?code=0408010AE&format=csv') 
 
 #-- Download gabapentin data (chemical designation only) --#
-# Total items prescribed under anti-epileptic drugs
+# Total items prescribed and quantity of tablets/capsules 
+# under anti-epileptic drugs
 # Code: 0408010G0
 gabapentin_epi <- read_csv('https://openprescribing.net/api/1.0/spending/?code=0408010G0&format=csv') 
 
-# Total items prescribed under drugs for neuropathic pain
+# Total items prescribed and quantity of tablets/capsules 
+# under drugs for neuropathic pain
 # Code: 0407030AD
 gabapentin_neup <- read_csv('https://openprescribing.net/api/1.0/spending/?code=0407030AD&format=csv') 
 
@@ -44,24 +46,30 @@ gabapentin <- gabapentin_epi %>%
 #-- Pregabalin --#
 pregabalin_clean <- pregabalin %>% 
     # Select columns
-    select(date, items) %>% 
+    select(date, items, quantity) %>% 
     # Filter date range: April 2017 to April 2021
     # Note: April 2021 was the last data point so only need to remove dates before April 2017.
-    filter(date >= as.Date('2017-04-01'))
+    filter(date >= as.Date('2017-04-01')) %>% 
+    # Calculate quantity per item
+    mutate(quantity_per_item = quantity / items)
 
 #-- Gabapentin --#
 gabapentin_clean <- gabapentin %>% 
     # Select columns
-    select(date, items.x, items.y) %>% 
+    select(date, items.x, items.y, quantity.x, quantity.y) %>% 
     # Filter date range: April 2017 to April 2021
     # Note: April 2021 was the last data point so only need to remove dates before April 2017.
     filter(date >= as.Date('2017-04-01')) %>% 
-    # Sum items columns from items.x (antiepileptic) and items.y (neuropathic pain)
     rowwise() %>% 
+    # Sum items columns from items.x (antiepileptic) and items.y (neuropathic pain)
     mutate(items = sum(c_across(cols = starts_with('items')), na.rm = TRUE)) %>% 
+    # Sum items columns from quantity.x (antiepileptic) and quantity.y (neuropathic pain)
+    mutate(quantity = sum(c_across(cols = starts_with('quantity')), na.rm = TRUE)) %>% 
     ungroup() %>% 
     # Select columns
-    select(date, items)
+    select(date, items, quantity) %>% 
+    # Calculate quantity per item
+    mutate(quantity_per_item = quantity / items)
 
 ####################
 #   Save to file   #
@@ -85,20 +93,26 @@ write_csv(x = gabapentin_clean,
 #-- Pregabalin --#
 pregabalin %>% 
     # Select columns
-    select(date, items) %>% 
+    select(date, items, quantity) %>% 
+    # Calculate quantity per item
+    mutate(quantity_per_item = quantity / items) %>% 
     # Write to file
     write_csv(file = 'data-clean/full-record_pregabalin.csv')
 
 #-- Gabapentin --#
 gabapentin %>% 
     # Select columns
-    select(date, items.x, items.y) %>% 
-    # Sum items columns from items.x (antiepileptic) and items.y (neuropathic pain)
+    select(date, items.x, items.y, quantity.x, quantity.y) %>%
     rowwise() %>% 
+    # Sum items columns from items.x (antiepileptic) and items.y (neuropathic pain)
     mutate(items = sum(c_across(cols = starts_with('items')), na.rm = TRUE)) %>% 
+    # Sum items columns from quantity.x (antiepileptic) and quantity.y (neuropathic pain)
+    mutate(quantity = sum(c_across(cols = starts_with('quantity')), na.rm = TRUE)) %>% 
     ungroup() %>% 
     # Select columns
-    select(date, items) %>% 
+    select(date, items, quantity) %>%
+    # Calculate quantity per item
+    mutate(quantity_per_item = quantity / items) %>% 
     # Write to file
     write_csv(file = 'data-clean/full-record_gabapentin.csv')
     
@@ -109,7 +123,7 @@ gabapentin %>%
 ############################################################
 
 # Fetching and cleaning the data only needs to be done once.
-# Date completed: 2021-06-18
+# Date completed: 2021-06-20
 
 ###############################################
 #   Fetch the data from openprescribing.net   #
@@ -119,16 +133,18 @@ gabapentin %>%
 # Drug codes can be obtained here: https://openprescribing.net/analyse/
 
 #-- Download pregabalin data (chemical designation only) --#
-# Total items prescribed
+# Total items prescribed and quantity of tablets/capsules
 # Code: 0408010AE
 pregabalin_ccg <- read_csv('https://openprescribing.net/api/1.0/spending_by_ccg/?code=0408010AE&format=csv') 
 
 #-- Download gabapentin data (chemical designation only) --#
-# Total items prescribed under anti-epileptic drugs
+# Total items prescribed uand quantity of tablets/capsules
+# under anti-epileptic drugs
 # Code: 0408010G0
 gabapentin_ccg_epi <- read_csv('https://openprescribing.net/api/1.0/spending_by_ccg/?code=0408010G0&format=csv') 
 
-# Total items prescribed under drugs for neuropathic pain
+# Total items prescribed and quantity of tablets/capsules
+# under drugs for neuropathic pain
 # Code: 0407030AD
 gabapentin_ccg_neup <- read_csv('https://openprescribing.net/api/1.0/spending_by_ccg/?code=0407030AD&format=csv') 
 
@@ -144,7 +160,9 @@ gabapentin_ccg <- gabapentin_ccg_epi %>%
 #-- Pregabalin --#
 pregabalin_ccg_clean <- pregabalin_ccg %>% 
     # Select columns
-    select(row_id, row_name, date, items) %>% 
+    select(row_id, row_name, date, items, quantity) %>% 
+    # Calculate quantity per item
+    mutate(quantity_per_item = quantity / items) %>% 
     # Filter date range: April 2017 to April 2021
     # Note: April 2021 was the last data point so only need to remove dates before April 2017.
     filter(date >= as.Date('2017-04-01'))
@@ -152,16 +170,20 @@ pregabalin_ccg_clean <- pregabalin_ccg %>%
 #-- Gabapentin --#
 gabapentin_ccg_clean <- gabapentin_ccg %>% 
     # Select columns
-    select(row_id, row_name, date, items.x, items.y) %>% 
+    select(row_id, row_name, date, items.x, items.y, quantity.x, quantity.y) %>% 
     # Filter date range: April 2017 to April 2021
     # Note: April 2021 was the last data point so only need to remove dates before April 2017.
     filter(date >= as.Date('2017-04-01')) %>% 
-    # Sum items columns from items.x (antiepileptic) and items.y (neuropathic pain)
     rowwise() %>% 
+    # Sum items columns from items.x (antiepileptic) and items.y (neuropathic pain)
     mutate(items = sum(c_across(cols = starts_with('items')), na.rm = TRUE)) %>% 
+    # Sum items columns from quantity.x (antiepileptic) and quantity.y (neuropathic pain)
+    mutate(quantity = sum(c_across(cols = starts_with('quantity')), na.rm = TRUE)) %>% 
     ungroup() %>% 
     # Select columns
-    select(row_id, row_name, date, items)
+    select(row_id, row_name, date, items, quantity) %>% 
+    # Calculate quantity per item
+    mutate(quantity_per_item = quantity / items)
 
 ####################
 #   Save to file   #
@@ -185,19 +207,25 @@ write_csv(x = gabapentin_ccg_clean,
 #-- Pregabalin --#
 pregabalin_ccg %>% 
     # Select columns
-    select(row_id, row_name, date, items) %>% 
+    select(row_id, row_name, date, items, quantity) %>% 
+    # Calculate quantity per item
+    mutate(quantity_per_item = quantity / items) %>% 
     # Write to file
     write_csv(file = 'data-clean/full-record_pregabalin_ccg.csv')
 
 #-- Gabapentin --#
 gabapentin_ccg %>% 
     # Select columns
-    select(row_id, row_name, date, items.x, items.y) %>% 
-    # Sum items columns from items.x (antiepileptic) and items.y (neuropathic pain)
+    select(row_id, row_name, date, items.x, items.y, quantity.x, quantity.y) %>% 
     rowwise() %>% 
+    # Sum items columns from items.x (antiepileptic) and items.y (neuropathic pain)
     mutate(items = sum(c_across(cols = starts_with('items')), na.rm = TRUE)) %>% 
+    # Sum items columns from quantity.x (antiepileptic) and quantity.y (neuropathic pain)
+    mutate(quantity = sum(c_across(cols = starts_with('quantity')), na.rm = TRUE)) %>% 
     ungroup() %>% 
     # Select columns
-    select(row_id, row_name, date, items) %>% 
+    select(row_id, row_name, date, items, quantity) %>% 
+    # Calculate quantity per item
+    mutate(quantity_per_item = quantity / items) %>% 
     # Write to file
     write_csv(file = 'data-clean/full-record_gabapentin_ccg.csv')
